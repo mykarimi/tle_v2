8434fba6436b1faa9d2bc3a463bfbc612a2aebbd
mykarimi <yassine.karimi@gmail.com>
2015-09-27 01:24:09 +0200

    Commit and upload main script

---
+++ tle_v2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import glob   
+import csv
+import fileinput
+import lxml
+import itertools
+from os import listdir
+from collections import Counter
+sys.path.append('/home/yassine/twtads/KafNafParserPy')
+from KafNafParserMod import *
+
+
+
+for files in os.listdir('input/'):
+
+	processed_biography = KafNafParser('input/'+files)    
+	target = open('termlayer.csv','w')
+
+	for term_obj in processed_biography.get_terms():
+    	print 
+
+		target_text = (term_obj.get_pos(), term_obj.get_lemma()) 
+		target.write(target_text.encode('utf-8'))	
+	target.close()
